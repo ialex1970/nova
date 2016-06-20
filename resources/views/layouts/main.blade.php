a969dc6da5c49e0fc898a38101184865851eb4fc
ialex1970 <ialex1970@hotmail.com>
2016-06-20 15:15:36 +0300

    styles & header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,11 +10,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/sl-slide.css">
+@include('layouts.partials.styles')
 
     <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
@@ -27,50 +23,7 @@
 </head>
 
 <body>
-
-<!--Header-->
-<header class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a id="logo" class="pull-left" href="index.html"></a>
-            <div class="nav-collapse collapse pull-right">
-                <ul class="nav">
-                    <li class="active"><a href="/">Home</a></li>
-                    <li><a href="about">About Us</a></li>
-                    <li><a href="services">Services</a></li>
-                    <li><a href="portfolio.html">Portfolio</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="career.html">Career</a></li>
-                            <li><a href="blog-item.html">Blog Single</a></li>
-                            <li><a href="faq.html">FAQ</a></li>
-                            <li><a href="pricing.html">Pricing</a></li>
-                            <li><a href="404.html">404</a></li>
-                            <li><a href="typography.html">Typography</a></li>
-                            <li><a href="registration.html">Registration</a></li>
-                            <li class="divider"></li>
-                            <li><a href="privacy.html">Privacy Policy</a></li>
-                            <li><a href="terms.html">Terms of Use</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="contact-us.html">Contact</a></li>
-                    <li class="login">
-                        <a data-toggle="modal" href="#loginForm"><i class="icon-lock"></i></a>
-                    </li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </div>
-</header>
-<!-- /header -->
-
+@include('layouts.partials.header')
 @yield('content')
 
 <!--Bottom-->
---
+++ resources/views/layouts/partials/styles.blade.php
@@ -0,0 +1,5 @@
+{!! Html::style('css/bootstrap.min.css') !!}
+{!! Html::style('css/bootstrap-responsive.min.css') !!}
+{!! Html::style('css/font-awesome.min.css') !!}
+{!! Html::style('css/main.css') !!}
+{!! Html::style('css/sl-slide.css') !!}
