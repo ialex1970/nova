4bdecc7e48e2d902c3e1e189894145083eefc027
ialex1970 <ialex1970@hotmail.com>
2016-06-20 18:25:50 +0300

    Models & migrations: category, article

---
+++ database/migrations/2016_06_20_152208_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('active');
+            $table->string('slug');
+            $table->string('title');
+            $table->text('short');
+            $table->text('full');
+            $table->string('image');
+            $table->integer('user_id')->unsigned();
+            $table->timestamp('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
